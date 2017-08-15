05581cd6e3f36994618f3323530b1a5b117ee1a8
lzdoublee <luthosanda@gmail.com>
2017-08-15 18:07:59 +0200

    Update PlayerStats.java

--- SoccerSystem/src/main/java/Domain/PlayerStats.java
+++ SoccerSystem/src/main/java/Domain/PlayerStats.java
@@ -3,7 +3,7 @@ package Domain;
 /**
  * Created by L.z Double E on 8/7/2017.
  */
-public class PlayerStats {
+public class PlayerStats implements Serializable{
 
     private String id;
     private int goalsScored;
