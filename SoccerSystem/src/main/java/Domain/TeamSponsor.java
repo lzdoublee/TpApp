8675d7052fea8a630d2b02ab174363397fd3efb2
lzdoublee <luthosanda@gmail.com>
2017-08-15 18:10:20 +0200

    Update TeamSponsor.java

--- SoccerSystem/src/main/java/Domain/TeamSponsor.java
+++ SoccerSystem/src/main/java/Domain/TeamSponsor.java
@@ -3,7 +3,7 @@ package Domain;
 /**
  * Created by L.z Double E on 8/7/2017.
  */
-public class TeamSponsor {
+public class TeamSponsor implements Serializable{
 
     private String location;
     private String fkSponsorId;
